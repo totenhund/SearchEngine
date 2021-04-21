78b105b17bc266be8b0694ac60dc806110ebd617
Kamil Khayrullin <Kamil Khayrullin@DESKTOP-FQSSIHS>
2021-04-21 20:48:50 +0300

    DBMS

--- src/com/company/database/DatabaseManager.java
+++ src/com/company/database/DatabaseManager.java
@@ -1,15 +1,39 @@
 package com.company.database;
 
 public class DatabaseManager {
-    public void saveWebsite(){
 
+    private static DatabaseManager instance;
+    public String value;
+
+    private DatabaseManager(String value) {
+        
+        this.value = value;
     }
 
+    public static DatabaseManager getInstance(String value) {
+        if (instance == null) {
+            instance = new DatabaseManager(value);
+        }
+        return instance;
+    }
+
+    public Website getWebsite(){
+
+    }
+    
+    public void postWebsite(){
+
+    }
+    
     public void updateRating(){
 
     }
+    
+    public void putWebsite(){
 
-    public void getFromQuery(){
+    }
+    
+    public Website deleteWebsite(){
 
     }
 }
