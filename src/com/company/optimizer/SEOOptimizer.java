903e2aa76a6f4548fceeea83035f169307369a4c
Kamil Khayrullin <Kamil Khayrullin@DESKTOP-FQSSIHS>
2021-04-21 21:00:29 +0300

    Optimizer

---
+++ src/com/company/optimizer/SEOOptimizer.java
@@ -0,0 +1,13 @@
+public class SEOOptimizer {
+    public void addWebsiteToDatabase(Website){
+
+    }
+
+    public JSON returnSEOStatistics(Website){
+
+    }
+
+    public JSON returnSEORecommendations(Website){
+
+    }
+}
